72b6cfd723f273d0b767386f603b877b24c7ec25
Bevziuk2005 <161725878+Bevziuk2005@users.noreply.github.com>
2024-06-22 11:25:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py migrate --fake program_web zero
-
-python manage.py makemigrations
-
 python manage.py migrate
