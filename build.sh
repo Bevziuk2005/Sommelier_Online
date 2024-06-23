2dfdd3c874a7ffaa2ce5313190cf299f98f0ae4b
User <nikitabevzuk124@gmail.com>
2024-06-23 20:02:30 +0300

    7

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Запуск gunicorn у фоновому режимі
 python -m gunicorn Sommelier_Online_Project.asgi:application -k uvicorn.workers.UvicornWorker &
 
-# Запуск телеграм-бота
 python manage.py program_tg
--- manage.py
+++ manage.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+#!/usr/bin/env python
 import os
 import sys
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sommelier_Online_Project.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Sommelier_Online_Project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +16,3 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    main()
